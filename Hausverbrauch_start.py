a44c4518e23040d158dcdf5210521788e6c09784
AStark76 <46598570+AStark76@users.noreply.github.com>
2022-12-14 12:21:17 +0100

    first window

---
+++ Hausverbrauch_start.py
@@ -0,0 +1,18 @@
+#!/python*
+
+import tkinter as tk
+from tkinter.ttk import *
+
+class Window(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title("Starks Hausverbrauchkontrolle")
+        
+        
+            
+
+
+
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
